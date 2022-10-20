fc1e477b977461c96d530e770a2794685994141e
Enes Demirbaş <enesdemirbasoffical@hotmail.com>
2022-10-20 03:32:22 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        //Java döngüler ile 0'dan girilen sayıya kadar olan sayılardan 3 ve 4'e tam bölünen sayıların ortalamasını hesaplayan programı yazınız.
+        Scanner inp=new Scanner(System.in);
+        int sayi,toplam=0,x=0,girilen;
+        System.out.print("Bir Sayı Giriniz: ");
+        girilen= inp.nextInt();
+        sayi=girilen;
+        for (;sayi>0;sayi--){
+            if (sayi%3==0){
+                if (sayi%4==0){
+                    toplam=(toplam+sayi);
+                    x++;
+                }
+            }
+        }
+        System.out.println("Sıfırdan "+girilen+" sayısına kadar olan sayılardan 3 ve 4'e tam bölünen sayıların ortalaması: "+toplam/x);
+
+
+    }
+}
